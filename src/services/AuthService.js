1e314c80a1b7ceefdf1004e42ab73ecbe875e2ce
AlexSensei <antishadow15@gmail.com>
2020-01-16 19:55:18 +0100

    Fixed code

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -3,7 +3,7 @@ import BaseApiService from './BaseApiService';
 const ENDPOINTS = {
   SIGN_IN: '/api/auth/login',
   SIGN_UP: '/api/auth/register',
-  LOGOUT: 'api/auth/logout'
+  LOGOUT: '/api/auth/logout'
 };
 
 class AuthService extends BaseApiService {
